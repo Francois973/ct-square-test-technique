8c465ab0f1eb4753b44af3e3eabd5670d111e91d
Francois973 <robino.francois973@gmail.com>
2023-04-04 14:53:50 +0200

    chore: add flatten_result in fetcher service

--- app/services/fetcher_service.rb
+++ app/services/fetcher_service.rb
@@ -8,6 +8,19 @@ class FetcherService
   def call
     url = URI("https://api.ipregistry.co/#{@ip}?key=#{ENV['API_KEY']}")
     response = Net::HTTP.get_response(url)
-    JSON.parse(response.body)
+    result = JSON.parse(response.body)
+
+    flatten_result(result)
+  end
+
+  def flatten_result(result)
+    result.each_with_object({}) do |(key, value), acc|
+      if value.is_a? Hash
+        next flatten_result(value).each do |k, v|
+          acc["#{key}.#{k}"] = v
+        end
+      end
+      acc[key] = value
+    end
   end
 end
